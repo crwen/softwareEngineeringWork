902210112d06e5428702bd75b23af3cd7ee76e03
crwen <1543720935@qq.com>
2019-09-24 17:24:46 +0800

    添加控制台传参功能

--- IndividualProject/WF.java
+++ IndividualProject/WF.java
@@ -8,7 +8,11 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * 字母类
@@ -21,35 +25,51 @@ public class WF {
 
 	private final static String STR = "abcdefghijklmnopqrstuvwxyz";
 
-	private final static String PATH = "D:\\a.txt";	//文件路径
-	
-	private static int num;	//记录字母的总个数
+	private static int num;	
 	
 	public static void main(String[] args) throws IOException {
 
-		DecimalFormat df = new DecimalFormat("######0.00%"); // 格式化
-
-		//获取文件内容并转化为小写
-		String content = read().toLowerCase();
-		
-		Alph[] arr = convert(content);
-		
-		System.out.println("字母\t出现次数\t频率");
-		for (int i = 0; i < STR.length(); ++i) {
-			System.out.println(arr[i].getCh() + "\t" + arr[i].getCnt() + "\t" + df.format((double)arr[i].getCnt() / num));
+		if (args.length > 1) {
+			String op = args[0];
+			if (op.equals("-c")) {
+				//获取文件路径
+				String path = getPath(args);
+				//获取文件内容
+				String content = read(path);
+				convert(content.toLowerCase());
+			} else if (op.equals("-f")) {
+				//获取文件路径
+				String path = getPath(args);
+				//获取文件内容
+				String content = read(path);
+				words(content);
+			}
 		}
-
 	}
 
+	/**
+	 * 获取文件路径
+	 * @param args
+	 * @return
+	 */
+	private static String getPath(String[] args) {
+		String path = "";
+		for (int i = 1; i < args.length; ++i) {
+			path += args[i] + " ";
+		}
+		
+		return path;
+	}
+	
 	/**
 	 * 读取文件
 	 * @return
 	 * @throws IOException
 	 */
-	private static String read() throws IOException  {
+	private static String read(String path) throws IOException  {
 
-		FileInputStream fip = new FileInputStream(PATH);
-		InputStreamReader reader = new InputStreamReader(fip, "utf-8");
+		FileInputStream fip = new FileInputStream(path);
+		InputStreamReader reader = new InputStreamReader(fip, "gbk");
 		StringBuffer sb = new StringBuffer();
 		while (reader.ready()) {
 			sb.append((char) reader.read());
@@ -68,7 +88,8 @@ public class WF {
 	 * @param content
 	 * @return
 	 */
-	private static Alph[] convert(String content) {
+	private static void convert(String content) {
+		DecimalFormat df = new DecimalFormat("######0.00%"); 
 		Alph[] arr = new Alph[26];
 		for (int i = 0; i < STR.length(); ++i) {
 			arr[i] = new Alph(STR.charAt(i));
@@ -82,10 +103,41 @@ public class WF {
 			}
 		}
 		
-		//对字母数组按照频率从大到小排序
+	
 		Arrays.sort(arr);
-		return arr;
+		
+		System.out.println("字母\t出现次数\t频率");
+		for (int i = 0; i < STR.length(); ++i) {
+			System.out.println(arr[i].getCh() + "\t" + arr[i].getCnt() + "\t" + df.format((double)arr[i].getCnt() / num));
+		}
+		
 	}
+	
+	
+	public static void words(String content) {
+		String strs[] = content.split(" ");
+		Set<String> set = new HashSet<String>();
+		SortedMap<String, Integer> map = new TreeMap<String, Integer>();
+
+		for (int i = 0; i < strs.length; ++i) {
+			if (strs[i].matches("[A-z]+[A-z0-9]")) {
+				if (map.get(strs[i]) == null) {
+					map.put(strs[i], 1);
+				} else {
+					map.put(strs[i], map.get(strs[i]) + 1);
+				}
+				
+//				set.add(strs[i]);
+			}
+		}
+		
+		System.out.println(map);
+//		Iterator<String> it = set.iterator();
+//		while(it.hasNext()) {
+//			System.out.println(it.next());
+//		}
+	}
+
 
 
 }
