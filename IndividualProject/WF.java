6d53fd6185f51f760f74ad490fec09323035e427
crwen <1543720935@qq.com>
2019-09-26 18:14:55 +0800

    完成step1 fun1

--- IndividualProject/WF.java
+++ IndividualProject/WF.java
@@ -1,82 +1,72 @@
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 /**
  * 字母类
  * @author crwen
  *
  */
-class Alph implements Comparable<Alph> {
-	private Character ch;		//字母
-	private Integer cnt;	//频数
-	
-	public Alph(char ch) {this.ch = ch; this.cnt = 0;}
-	
-	public void setCnt(int cnt) { this.cnt = cnt; }
-	
-	public void setCh(char ch) { this.ch = ch; }
-	
-	public int getCnt() {return this.cnt; }
-	
-	public char getCh() {return this.ch; }
 
-	/**
-	 * 重写compare方法，使之按照频率从大到小排列
-	 * 如果频数一样，就按字典序排列
-	 */
-	@Override
-	public int compareTo(Alph o) {
-		if (o.cnt != this.cnt) {
-			return o.cnt.compareTo(this.cnt);
-		} else {
-			return this.ch.compareTo(o.ch);
-		}
-	}
-
-}
 
 public class WF {
 
 	private final static String STR = "abcdefghijklmnopqrstuvwxyz";
 
-	private final static String PATH = "D:\\a.txt";	//文件路径
-	
-	private static int num;	//记录字母的总个数
+	private static int num;	
 	
 	public static void main(String[] args) throws IOException {
 
-		DecimalFormat df = new DecimalFormat("######0.00%"); // 格式化
-
-		//获取文件内容并转化为小写
-		String content = read().toLowerCase();
-		
-		Alph[] arr = convert(content);
-		
-		System.out.println("字母\t出现次数\t频率");
-		for (int i = 0; i < STR.length(); ++i) {
-			System.out.println(arr[i].getCh() + "\t" + arr[i].getCnt() + "\t" + df.format((double)arr[i].getCnt() / num));
+		if (args.length > 1) {
+			String op = args[0];
+			if (op.equals("-c")) {
+				//获取文件路径
+				String path = getPath(args);
+				//获取文件内容
+				String content = read(path);
+				convert(content.toLowerCase());
+			} else if (op.equals("-f")) {
+				//获取文件路径
+				String path = getPath(args);
+				//获取文件内容
+				String content = read(path);
+				words(content);
+			}
 		}
-
 	}
 
+	/**
+	 * 获取文件路径
+	 * @param args
+	 * @return
+	 */
+	private static String getPath(String[] args) {
+		String path = "";
+		for (int i = 1; i < args.length; ++i) {
+			path += args[i] + " ";
+		}
+		
+		return path;
+	}
+	
 	/**
 	 * 读取文件
 	 * @return
 	 * @throws IOException
 	 */
-	private static String read() throws IOException  {
+	private static String read(String path) throws IOException  {
 
-		FileInputStream fip = new FileInputStream(PATH);
-		InputStreamReader reader = new InputStreamReader(fip, "utf-8");
+		FileInputStream fip = new FileInputStream(path);
+		InputStreamReader reader = new InputStreamReader(fip, "gbk");
 		StringBuffer sb = new StringBuffer();
 		while (reader.ready()) {
 			sb.append((char) reader.read());
@@ -95,7 +85,8 @@ public class WF {
 	 * @param content
 	 * @return
 	 */
-	private static Alph[] convert(String content) {
+	private static void convert(String content) {
+		DecimalFormat df = new DecimalFormat("######0.00%"); 
 		Alph[] arr = new Alph[26];
 		for (int i = 0; i < STR.length(); ++i) {
 			arr[i] = new Alph(STR.charAt(i));
@@ -109,10 +100,49 @@ public class WF {
 			}
 		}
 		
-		//对字母数组按照频率从大到小排序
+	
 		Arrays.sort(arr);
-		return arr;
+		
+		System.out.println("字母\t出现次数\t频率");
+		for (int i = 0; i < STR.length(); ++i) {
+			System.out.println(arr[i].getCh() + "\t" + arr[i].getCnt() + "\t" + df.format((double)arr[i].getCnt() / num));
+		}
+		
 	}
+	
+	
+	public static void words(String content) {
+		DecimalFormat df = new DecimalFormat("######0.00%"); 
+		String strs[] = content.split(" ");
+		Set<Alph> set = new HashSet<Alph>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < strs.length; ++i) {
+			if (strs[i].matches("[A-z]+[A-z0-9]")) {
+				num++;
+				if (map.get(strs[i]) == null) {
+					map.put(strs[i], 1);
+				} else {
+					map.put(strs[i], map.get(strs[i]) + 1);
+				}
+			}
+		}
+		
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			set.add(new Alph(entry.getKey(), entry.getValue()));
+//			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+
+		Alph[] arr =  new Alph[set.size()];
+		set.toArray(arr);
+		Arrays.sort(arr);
+		
+		System.out.println("单词   \t\t   频率");
+		for (int i = 0; i < arr.length; ++i) {
+			System.out.println(arr[i].getWord() + "\t\t" +  df.format((double)arr[i].getCnt() / num));
+			
+		}
+	}
+
 
 
 }
