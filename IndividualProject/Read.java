28583d1698936f65cb9f2872ee0f67b97c8fce04
crwen <1543720935@qq.com>
2019-09-26 20:13:37 +0800

    提取Read类，读取文件，并添加读取文件夹方法

---
+++ IndividualProject/Read.java
@@ -0,0 +1,63 @@
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 文件读写的工具类
+ * @author crwen
+ *
+ */
+public class Read {
+	
+	private static List<String> fileList = new ArrayList<>();
+	
+	/**
+	 * 读取文件
+	 * @return
+	 * @throws IOException
+	 */
+	public static String readFile(String path) throws IOException  {
+
+		FileInputStream fip = new FileInputStream(path);
+		InputStreamReader reader = new InputStreamReader(fip, "gbk");
+		StringBuffer sb = new StringBuffer();
+		while (reader.ready()) {
+			sb.append((char) reader.read());
+		}
+//	    System.out.println(sb.toString());
+		reader.close();
+		fip.close();
+		
+		//返回文件内容
+		return sb.toString();
+	}
+	
+	public static List<String> readDirection(String path) throws IOException {
+		
+		File file = new File(path);
+		File[] files = file.listFiles();
+		
+		for (int i = 0; i < files.length; ++i) {
+			if (files[i].isDirectory()) {
+				readDirection(files[i].getAbsolutePath());
+			} else {
+				fileList.add( readFile(files[i].getAbsolutePath()) );
+//				System.out.println(files[i]);
+			}
+			
+		}
+		System.out.println();
+		
+		
+		
+		return fileList;
+	}
+	
+	
+
+}
